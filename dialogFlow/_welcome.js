410bd33eaf9c97af5be571be2ccebfdce16822a4
VyTorBrB <viktor_vicente2@hotmail.com>
2019-04-05 19:56:43 -0300

    Re-arranged _welcome

--- dialogFlow/_welcome.js
+++ dialogFlow/_welcome.js
@@ -1,6 +1,7 @@
 module.exports = function (bot) {
     bot.dialog('/welcome', (session, results) => {
         session.send('Pizzaria 2 sabores Bem Vindo!')
-        session.send('Em que posso ajudalo?')
+        session.send('Voce pode pedir o menu do dia ou pedir direto pelo que buscas! :)')
+        session.send('Entao, Em que posso ajudalo?')
     }).triggerAction({ matches: /^(oi|ola|tudo bem|como vai|como esta)/i })
 }
