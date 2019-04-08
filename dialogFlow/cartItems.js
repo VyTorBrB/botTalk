6a462ed03fa0c2f60076e2b0914785ddca8cb895
VyTorBrB <viktor_vicente2@hotmail.com>
2019-04-08 13:05:27 -0300

    Working sumtotals for each cardShow

--- dialogFlow/cartItems.js
+++ dialogFlow/cartItems.js
@@ -16,6 +16,83 @@ module.exports = function (bot, builder) {
             price: 7.00,
             regex: /^(sc02|SC02)/i,
             total: []
+        },
+        {
+            id: 'sc03',
+            title: 'Suco dell valle - Morango',
+            price: 8.33,
+            regex: /^(sc03|SC03)/i,
+            total: []
+        },
+        {
+            id: 'sc04',
+            title: 'Suco dell valle - Pessego',
+            price: 5.00,
+            regex: /^(sc04|SC04)/i,
+            total: []
+        },
+        {
+            id: 'sc05',
+            title: 'Suco dell valle - Limao',
+            price: 10.90,
+            regex: /^(sc05|SC05)/i,
+            total: []
+        },
+        {
+            id: 'pz01',
+            title: 'Pizza Calabresa',
+            price: 25.00,
+            regex: /^(pz01|PZ01)/i,
+            total: []
+        },
+        {
+            id: 'pz02',
+            title: 'Pizza Vegetariana',
+            price: 30.00,
+            regex: /^(pz02|PZ02)/i,
+            total: []
+        },
+        {
+            id: 'pz03',
+            title: 'Pizza Gourmet',
+            price: 5.00,
+            regex: /^(pz03|PZ03)/i,
+            total: []
+        },
+        {
+            id: 'pz04',
+            title: 'Pizza 3 queijos',
+            price: 15.00,
+            regex: /^(pz04|PZ04)/i,
+            total: []
+        },
+        {
+            id: 'pz05',
+            title: 'Pizza Pao com Ovo',
+            price: 90,
+            regex: /^(pz05|PZ05)/i,
+            total: []
+        },
+        {
+            id: 'cd01',
+            title: 'Doce-de-leite',
+            price: 4.00,
+            regex: /^(cd01|CD01)/i,
+            total: []
+        },
+        {
+            id: 'cd02',
+            title: 'Algodao',
+            price: 4.00,
+            regex: /^(cd02|CD02)/i,
+            total: []
+        },
+        {
+            id: 'cd03',
+            title: 'Manteiga de amendoim',
+            price: 10.00,
+            regex: /^(cd03|CD03)/i,
+            total: []
         }
     ]
     const createRoutes = cartId => {
